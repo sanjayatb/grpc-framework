b57b01e2d79a59149bbc031fc3d113d2bb3f337e
Sanjaya Rajamanthri <31567956+sanjayatb@users.noreply.github.com>
2020-12-20 11:51:52 +0800

    Grpc session class

--- grpc-client/src/main/java/com/stb/grpc/client/GrpcSession.java
+++ grpc-client/src/main/java/com/stb/grpc/client/GrpcSession.java
@@ -1,10 +1,57 @@
 package com.stb.grpc.client;
 
 import com.stb.grpc.client.channel.Channel;
+import com.stb.grpc.client.channel.LoadBalanceType;
+import com.stb.grpc.model.GrpcGenericServiceGrpc;
 
+//TODO complete implementation with channel
+/***
+ * Grpc session creation
+ */
 public class GrpcSession {
 
     private Channel channel;
+    private GrpcGenericServiceGrpc.GrpcGenericServiceFutureStub futureStub;
+    private static GrpcSession DEFAULT_SESSION = new GrpcSession(new Builder());
+    private GrpcSession grpcSession;
+
+    public GrpcSession getActiveSession(){
+        return grpcSession;
+    }
+
+    private GrpcSession(Builder builder){
+        this.grpcSession = builder.grpcSession;
+    }
+
+    public static final class Builder {
+        private String hostNames = "";
+        private int port = 0;
+        private LoadBalanceType loadBalanceType;
+        private GrpcSession grpcSession;
+
+        public Builder setPort(int port){
+            this.port = port;
+            return this;
+        }
+
+        public Builder setHostNames(String hostNames) {
+            this.hostNames = hostNames;
+            return this;
+        }
+
+        public Builder setLoadBalanceType(LoadBalanceType loadBalanceType) {
+            this.loadBalanceType = loadBalanceType;
+            return this;
+        }
+
+        public GrpcSession getOrCreate(){
+            if(null == this.grpcSession){
+                return new GrpcSession(this);
+            }
+            return grpcSession;
+        }
+    }
+
 
 
 
